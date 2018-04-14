83b59ec57b1edf9f745d5336310246e92ca5c791
dward1502@gmail.com <dward1502@gmail.com>
2018-04-13 17:47:28 -0700

    worked on styling of registration modal

--- spa-store/client/src/Components/Registration/Registration.js
+++ spa-store/client/src/Components/Registration/Registration.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Input from '../UI/Input/Input';
 import classes from './registration.css';
+import logoImg from "../../assets/Images/reactBP.png";
 class Registration extends Component {
    state={
       name: '',
@@ -9,13 +10,48 @@ class Registration extends Component {
       username: ''
    }
    render() {
+       var style = {
+           image: {
+               width: 125,
+               height: 100,               
+           }
+       }
       let form = (
          <form>
-            <Input inputtype='register' type='text' name='name' placeholder='Full Name' />
-            <Input inputtype='register' type ='email' name='email' placeholder='Email' />
-            <Input inputtype='register' type='username' name='username' placeholder='username' />
-            <Input inputtype='register' type='password' name='password' placeholder='password'/>
-            
+             <div className={classes.LogoModal}>
+                 <i><img src={logoImg} style={style.image} alt='logo' /></i>
+             </div>
+             <span className={classes.TitleModal}>Sign Up</span>
+             <div>
+                 <h1>Google sign in box</h1>
+             </div>
+              <div className='Or'>
+                  <h2><span>Or</span></h2>
+              </div>
+              <Input inputtype='register'
+                     type='text' 
+                     name='name' 
+                     label='Full Name'
+                     placeholder='Full Name' />
+              <Input inputtype='register' 
+                    //  icon='fas fa-envelope' 
+                     label='Email' 
+                     type ='email' 
+                     name='email' 
+                     placeholder='Email' />
+            <Input inputtype='register' 
+                    //  icon='fas fa-user' 
+                     label='Username' 
+                     type='username' 
+                     name='username' 
+                      placeholder='username' />
+              <Input inputtype='register' 
+                    //  icon='fas fa-lock' 
+                     label='Password' 
+                     type='password' 
+                     name='password' 
+                     placeholder='password'/>
+                    
          </form>
       )
       return (
--- spa-store/client/src/Components/UI/Input/Input.js
+++ spa-store/client/src/Components/UI/Input/Input.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import classes from './Input.css';
 
+
 const input = (props) => {
    let inputEl = null;
-
+   
    switch (props.inputtype){
       case('login'):
          inputEl = <input className={classes.InputEl} {...props}/>
          break;
       case('register'):
-         inputEl = <input className={classes.InputEl} {...props}/>
+         inputEl = (
+               <div>
+                  <div className={classes.InputDiv}>
+                  <span className={props.icon}></span>
+                        <input className={classes.InputEl} {...props}/>
+                  </div>
+               </div>
+            )
          break;
       default:
          inputEl = <input className={classes.InputEl} {...props}/>
