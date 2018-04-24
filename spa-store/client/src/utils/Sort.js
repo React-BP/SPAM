3fe0b1cab82801199f4626a06a8490d9cf4b4e2b
Mateo Vargas <mateovargas5@gmail.com>
2018-04-23 22:10:41 -0700

    Added mapping func.

--- spa-store/client/src/utils/Sort.js
+++ spa-store/client/src/utils/Sort.js
@@ -3,12 +3,12 @@ import lodash from 'lodash';
 export default {
     sortLowtoHigh: (list) => {
 
-        _.sortBy(list, ['price', 'name']);
+        lodash.sortBy(list, ['price', 'name']);
 
        console.log(list);
     },
     sortHightoLow: (list) => {
-        _.sortBy(list, ['price', 'name']);
+        lodash.sortBy(list, ['price', 'name']);
         return list.reverse();
     },
     filterByGender: (list, gender) => {
