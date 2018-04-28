950584c209c838d39705cd36da1abad4b5add33a
Mateo Vargas <mateovargas5@gmail.com>
2018-04-27 21:04:24 -0700

    working on checkout routing

--- spa-store/controllers/modelController.js
+++ spa-store/controllers/modelController.js
@@ -27,4 +27,35 @@ module.exports = {
             .then((items) => {res.json(items);})
                .catch(err => res.status(422).json(err));
     },
+    updateItem: function(req, res){
+        console.log('update item id is: ', req.params.id);
+        db.Item.findByIdAndUpdate(req.params.id, JSON.parse(req.data.item))
+               .then((item) => res.json(item))
+               .catch(err => res.status(422).json(err));
+    },
+    findOrder: function(req, res){
+        console.log('finding order');
+        db.Order.find()
+                .where('user')
+                .equals(req.data.id)
+                .then((order) => res.json(order))
+                .catch(err => res.status(422).json(err));
+    },
+    findOrderById: function(req, res){
+        console.log('id is: ', req.params.id);
+        db.Order.findById(req.params.id)
+                .then((order) => res.json(order))
+                .catch(err => res.status(422).json(err));
+    },
+    createOrder: function(req, res){
+        
+        db.Order.create(JSON.parse(req.data.item))
+                .then((order) => res.json(order))
+                .catch(err => res.status(422).json(err));
+    },
+    updateOrder: function(req, res){
+        console.log('id is: ', req.params.id);
+        db.Order.findByIdAndUpdate(req.params.id, JSON.parse(req.data.order))
+            .then((order) => res.json(order))
+    }
 }
